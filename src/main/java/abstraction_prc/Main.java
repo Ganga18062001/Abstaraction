8734351e1d4f34bc9563f3d07c18131ad3ecfb5e
Ganga18062001 <gangashirale@gmail.com>
2026-02-20 13:38:00 +0530

    one more class is added

--- src/main/java/abstraction_prc/Main.java
+++ src/main/java/abstraction_prc/Main.java
@@ -30,14 +30,22 @@ class Scooter extends Vehicle{
     }
 
 }
+class MotarCycle extends Vehicle{
+    void start(){
+        System.out.println("MotartCycle is start");
+    }
+    void Racea(){
+        System.out.println("Motar race");
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
         Truck v1 = new Truck();
-//        v1.start();
-//        v1.No_of_vehicle();
         v1.start();
         v1.No_of_vehicle();
+//        v1.start();
+//        v1.No_of_vehicle();
 
         Scooter v2 = new Scooter();
         v2.start();
